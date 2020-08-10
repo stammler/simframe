a583e61821254af744ee33ed3ebf4a81993c086a
Sebastian Stammler <sebastian.stammler@gmail.com>
2020-08-10 20:04:16 +0200

    Added more integration schemes

--- simframe/integration/schemes/expl_2_fehlberg_adptv.py
+++ simframe/integration/schemes/expl_2_fehlberg_adptv.py
@@ -3,11 +3,15 @@ from simframe.integration import AbstractScheme
 import numpy as np
 
 # Butcher coefficients
-b0, b1  =  1/2, 1/2
-e0, e1  = -1/2, 1/2
+a10           = 1/2
+a20, a21      = 1/256 , 255/256
+b0 , b1 , b2  = 1/512 , 255/256, 1/512
+bs0, bs1      = 1/256 , 255/256
+e0 , e1 , e2  = b0-bs0, b1-bs1 , b2
+c1            = 1/2
 
-def _f_expl_2_heun_euler_adptv(x0, Y0, dx, *args, econ=0.0324, eps=0.1, pgrow=-0.5, pshrink=-1., safety=0.9, **kwargs):
-    """Explicit adaptive 2nd-order Heun-Euler method
+def _f_expl_2_fehlberg_adptv(x0, Y0, dx, *args, econ=0.0324, eps=0.1, pgrow=-0.5, pshrink=-1., safety=0.9, **kwargs):
+    """Explicit adaptive 2nd-order Fehlberg's method
     
     Parameters
     ----------
@@ -38,18 +42,20 @@ def _f_expl_2_heun_euler_adptv(x0, Y0, dx, *args, econ=0.0324, eps=0.1, pgrow=-0
         
     Butcher tableau
     ---------------
-      0  |  0   0
-      1  |  1   0
-    -----|---------
-         | 1/2 1/2
-         |  1   0
+      0  | 0     0       0
+     1/2 | 1/2   0       0
+      1  | 1/256 255/256 0
+    -----|---------------------
+         | 1/512 255/256 1/512
+         | 1/256 255/256 0
     """
-    k0 = Y0.derivative(x0     , Y0        )
-    k1 = Y0.derivative(x0 + dx, Y0 + k0*dx)
+    k0 = Y0.derivative(x0        , Y0                       )
+    k1 = Y0.derivative(x0 + c1*dx, Y0 +  a10*k0          *dx)
+    k2 = Y0.derivative(x0 +    dx, Y0 + (a20*k0 + a21*k1)*dx)
 
     Yscale  = np.abs(Y0) + np.abs(dx*k0)
 
-    e = dx*(e0*k0 + e1*k1)
+    e = dx*(e0*k0 + e1*k1 + e2*k2)
     emax = np.max(np.abs(e/Yscale)) / eps
 
     # Integration successful
@@ -57,11 +63,11 @@ def _f_expl_2_heun_euler_adptv(x0, Y0, dx, *args, econ=0.0324, eps=0.1, pgrow=-0
         # Suggest new stepsize
         dxnew = safety*dx*emax**pgrow if econ > emax else 5.*dx
         x0.suggest(dxnew)
-        return dx*(b0*k0 + b1*k1)
+        return dx*(b0*k0 + b1*k1 + b2*k2)
     else:
         # Suggest new stepsize
         dxnew = np.maximum(safety*dx*emax**pshrink, 0.1*dx)
         x0.suggest(dxnew)
         return False
 
-expl_2_heun_euler_adptv = AbstractScheme(_f_expl_2_heun_euler_adptv, description="Explicit adaptive 2nd-order Heun-Euler method")
+expl_2_fehlberg_adptv = AbstractScheme(_f_expl_2_fehlberg_adptv, description="Explicit adaptive 2nd-order Fehlberg's method")
--- simframe/integration/schemes/expl_3_bogacki_shampine_adptv.py
+++ simframe/integration/schemes/expl_3_bogacki_shampine_adptv.py
@@ -3,11 +3,16 @@ from simframe.integration import AbstractScheme
 import numpy as np
 
 # Butcher coefficients
-b0, b1  =  1/2, 1/2
-e0, e1  = -1/2, 1/2
+a10                = 1/2
+a21                = 3/4
+a30, a31, a32      = 2/9   , 1/3   , 4/9
+b0 , b1 , b2       = 2/9   , 1/3   , 4/9
+bs0, bs1, bs2, bs3 = 7/24  , 1/4   , 1/3   , 1/8
+e0 , e1 , e2 , e3  = b0-bs0, b1-bs1, b2-bs2, -bs3
+c1 , c2            = 1/2   , 3/4
 
-def _f_expl_2_heun_euler_adptv(x0, Y0, dx, *args, econ=0.0324, eps=0.1, pgrow=-0.5, pshrink=-1., safety=0.9, **kwargs):
-    """Explicit adaptive 2nd-order Heun-Euler method
+def _f_expl_3_bogacki_shampine_adptv(x0, Y0, dx, *args, econ=0.005832, eps=0.1, pgrow=-1/3, pshrink=-0.5, safety=0.9, **kwargs):
+    """Explicit adaptive 3rd-order Bogacki-Shampine method
     
     Parameters
     ----------
@@ -17,13 +22,13 @@ def _f_expl_2_heun_euler_adptv(x0, Y0, dx, *args, econ=0.0324, eps=0.1, pgrow=-0
         Variable to be integrated at the beginning of scheme
     dx : IntVar
         Stepsize of integration variable
-    econ : float, optional, default : 0.0324
+    econ : float, optional, default : 0.005832
         Error controll parameter for setting stepsize
     eps : float, optional, default : 0.9
         Desired maximum relative error
-    prgrow : float, optional, default : -0.5
+    prgrow : float, optional, default : -1/3
         Power for increasing step size
-    pshrink : float, optional, default : -1.
+    pshrink : float, optional, default : -1/2
         Power for decreasing stepsize
     safety : float, optional, default : 0.9
         Safety factor when changing step size
@@ -38,18 +43,22 @@ def _f_expl_2_heun_euler_adptv(x0, Y0, dx, *args, econ=0.0324, eps=0.1, pgrow=-0
         
     Butcher tableau
     ---------------
-      0  |  0   0
-      1  |  1   0
-    -----|---------
-         | 1/2 1/2
-         |  1   0
+      0  |  0    0   0   0
+     1/2 | 1/2   0   0   0
+     3/4 |  0   3/4  0   0
+      1  | 2/9  1/3 4/9  0
+    -----|------------------
+         | 2/9  1/3 4/9  0
+         | 7/24 1/4 1/3 1/8
     """
-    k0 = Y0.derivative(x0     , Y0        )
-    k1 = Y0.derivative(x0 + dx, Y0 + k0*dx)
+    k0 = Y0.derivative(x0        , Y0                                 )
+    k1 = Y0.derivative(x0 + c1*dx, Y0 +   a10*k0                   *dx)
+    k2 = Y0.derivative(x0 + c2*dx, Y0 +            a21*k1          *dx)
+    k3 = Y0.derivative(x0 +    dx, Y0 +  (a30*k0 + a31*k1 + a32*k2)*dx)
 
     Yscale  = np.abs(Y0) + np.abs(dx*k0)
 
-    e = dx*(e0*k0 + e1*k1)
+    e = dx*(e0*k0 + e1*k1 + e2*k2 + e3*k3)
     emax = np.max(np.abs(e/Yscale)) / eps
 
     # Integration successful
@@ -57,11 +66,11 @@ def _f_expl_2_heun_euler_adptv(x0, Y0, dx, *args, econ=0.0324, eps=0.1, pgrow=-0
         # Suggest new stepsize
         dxnew = safety*dx*emax**pgrow if econ > emax else 5.*dx
         x0.suggest(dxnew)
-        return dx*(b0*k0 + b1*k1)
+        return dx*(b0*k0 + b1*k1 + b2*k2)
     else:
         # Suggest new stepsize
         dxnew = np.maximum(safety*dx*emax**pshrink, 0.1*dx)
         x0.suggest(dxnew)
         return False
 
-expl_2_heun_euler_adptv = AbstractScheme(_f_expl_2_heun_euler_adptv, description="Explicit adaptive 2nd-order Heun-Euler method")
+expl_3_bogacki_shampine_adptv = AbstractScheme(_f_expl_3_bogacki_shampine_adptv, description="Explicit adaptive 3rd-order Bogacki-Shampine method")
