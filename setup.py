6c511b6d03210b921a8aa5698781c49727cc3ab0
Sebastian Stammler <sebastian.stammler@gmail.com>
2020-08-17 20:02:17 +0200

    Added scipy to requirements for GMRES solver

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author_email="stammler@usm.lmu.de; til.birnstiel@lmu.de",
     packages=find_packages(),
     license="GPLv3",
-    install_requires=["dill", "h5py", "numpy", "pytest"],
+    install_requires=["dill", "h5py", "numpy", "pytest", "scipy"],
     include_package_data=True,
     zip_safe=False,
     version=read_version(),
