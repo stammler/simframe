aba337c854cfc3af4ccf78f2deb068757d449dc3
Sebastian Stammler <sebastian.stammler@gmail.com>
2020-08-18 18:36:41 +0200

    Version numbering

--- simframe/__init__.py
+++ simframe/__init__.py
@@ -5,7 +5,7 @@ from simframe.integration import schemes
 from simframe.io import writers
 
 __name__ = "simframe"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = ["Frame",
            "Instruction",
