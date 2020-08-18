c87dbc7f3d0a1a9d4f9443495f9ba49c4d32581b
Sebastian Stammler <sebastian.stammler@gmail.com>
2020-08-18 16:33:23 +0200

    Version change

--- simframe/__init__.py
+++ simframe/__init__.py
@@ -5,7 +5,7 @@ from simframe.integration import schemes
 from simframe.io import writers
 
 __name__ = "simframe"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = ["Frame",
            "Instruction",
