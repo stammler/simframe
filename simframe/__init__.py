43170a5698122bd5b4c5fb37f4fe50e6a1e7ad9d
Sebastian Stammler <sebastian.stammler@gmail.com>
2020-11-03 16:07:17 +0100

    Change to version number 0.4.13

--- simframe/__init__.py
+++ simframe/__init__.py
@@ -5,7 +5,7 @@ from simframe.integration import schemes
 from simframe.io import writers
 
 __name__ = "simframe"
-__version__ = "0.4.12"
+__version__ = "0.4.13"
 
 __all__ = ["Frame",
            "Instruction",
