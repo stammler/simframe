fa9d319082bcfd801aee013757bea891192e9c48
Sebastian Stammler <sebastian.stammler@gmail.com>
2021-01-21 17:35:36 +0100

    Update to version 0.4.18

--- simframe/__init__.py
+++ simframe/__init__.py
@@ -5,7 +5,7 @@ from simframe.integration import schemes
 from simframe.io import writers
 
 __name__ = "simframe"
-__version__ = "0.4.17"
+__version__ = "0.4.18"
 
 __all__ = ["Frame",
            "Instruction",
